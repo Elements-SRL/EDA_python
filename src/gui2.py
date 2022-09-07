853d6f2d217cd2a14b849805cf8e574a77a3f6b3
Luca Rossi <lucarossi147@gmail.com>
2022-09-07 17:36:01 +0200

    added layout and frames to gui2

--- src/gui2.py
+++ src/gui2.py
@@ -1,7 +1,7 @@
 import sys
 from PyQt5 import QtWidgets
 from PyQt5.QtCore import QRect, QMetaObject, QCoreApplication
-from PyQt5.QtWidgets import QWidget, QMenuBar, QStatusBar, QMenu, QAction, QFileDialog
+from PyQt5.QtWidgets import QWidget, QMenuBar, QStatusBar, QMenu, QAction, QFileDialog, QGridLayout, QFrame
 import logics
 
 
@@ -9,6 +9,11 @@ class UiMainWindow(object):
     logics = None
 
     def __init__(self):
+        self.menu_export_as = None
+        self.frame = None
+        self.frame_2 = None
+        self.gridLayout = None
+        self.action_csv = None
         self.status_bar = None
         self.menu_file = None
         self.menubar = None
@@ -16,34 +21,56 @@ class UiMainWindow(object):
         self.actionOpen = None
         self.logics = logics.Logics()
 
-    def setup_ui(self, MainWindow):
-        if not MainWindow.objectName():
-            MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(800, 600)
-        self.actionOpen = QAction(MainWindow)
+    def setup_ui(self, main_window):
+        if not main_window.objectName():
+            main_window.setObjectName(u"MainWindow")
+        main_window.resize(800, 600)
+        self.actionOpen = QAction(main_window)
         self.actionOpen.setObjectName(u"actionOpen")
-        self.central_widget = QWidget(MainWindow)
+        self.action_csv = QAction(main_window)
+        self.action_csv.setObjectName(u"action_csv")
+        self.central_widget = QWidget(main_window)
         self.central_widget.setObjectName(u"centralwidget")
-        MainWindow.setCentralWidget(self.central_widget)
-        self.menubar = QMenuBar(MainWindow)
+        self.gridLayout = QGridLayout(self.central_widget)
+        self.gridLayout.setObjectName(u"gridLayout")
+        self.frame_2 = QFrame(self.central_widget)
+        self.frame_2.setObjectName(u"frame_2")
+        self.frame_2.setFrameShape(QFrame.StyledPanel)
+        self.frame_2.setFrameShadow(QFrame.Raised)
+
+        self.gridLayout.addWidget(self.frame_2, 1, 0, 1, 1)
+
+        self.frame = QFrame(self.central_widget)
+        self.frame.setObjectName(u"frame")
+        self.frame.setFrameShape(QFrame.StyledPanel)
+        self.frame.setFrameShadow(QFrame.Raised)
+
+        self.gridLayout.addWidget(self.frame, 0, 0, 1, 1)
+
+        main_window.setCentralWidget(self.central_widget)
+        self.menubar = QMenuBar(main_window)
         self.menubar.setObjectName(u"menubar")
-        self.menubar.setGeometry(QRect(0, 0, 800, 32))
+        self.menubar.setGeometry(QRect(0, 0, 722, 32))
         self.menu_file = QMenu(self.menubar)
         self.menu_file.setObjectName(u"menufile")
-        MainWindow.setMenuBar(self.menubar)
-        self.status_bar = QStatusBar(MainWindow)
+        self.menu_export_as = QMenu(self.menu_file)
+        self.menu_export_as.setObjectName(u"menuExport_as")
+        main_window.setMenuBar(self.menubar)
+        self.status_bar = QStatusBar(main_window)
         self.status_bar.setObjectName(u"statusbar")
-        MainWindow.setStatusBar(self.status_bar)
+        main_window.setStatusBar(self.status_bar)
 
         self.menubar.addAction(self.menu_file.menuAction())
         self.menu_file.addAction(self.actionOpen)
+        self.menu_file.addAction(self.menu_export_as.menuAction())
+        self.menu_export_as.addAction(self.action_csv)
 
-        self.retranslate_ui(MainWindow)
+        self.retranslate_ui(main_window)
 
-        QMetaObject.connectSlotsByName(MainWindow)
+        QMetaObject.connectSlotsByName(main_window)
 
-        self.actionOpen.triggered.connect(lambda: self.get_file())
-
+        self.actionOpen.triggered.connect(lambda: self.open())
+        self.action_csv.triggered.connect(lambda: self.csv())
     # setupUi
 
     def retranslate_ui(self, MainWindow):
@@ -55,17 +82,22 @@ class UiMainWindow(object):
         # if QT_CONFIG(shortcut)
         self.actionOpen.setShortcut(QCoreApplication.translate("MainWindow", u"Ctrl+O", None))
         # endif // QT_CONFIG(shortcut)
+        self.action_csv.setText(QCoreApplication.translate("MainWindow", u".csv", None))
         self.menu_file.setTitle(QCoreApplication.translate("MainWindow", u"File", None))
-
+        self.menu_export_as.setTitle(QCoreApplication.translate("MainWindow", u"Export as ...", None))
     # retranslateUi
 
-    def get_file(self):
+    def open(self):
         f_name, _ = QFileDialog.getOpenFileName(self.central_widget, 'Open file',
                                                 filter="Abf files (*.abf);; Edh files(*.edh)")
         self.logics.open_abf(f_name)
         # print last abf read
         print(self.logics.abfs[len(self.logics.abfs)-1])
         print(len(self.logics.abfs))
+
+    def csv(self):
+        # TODO
+        print("exporting csv... ")
 
 
 if __name__ == '__main__':
@@ -74,4 +106,4 @@ if __name__ == '__main__':
     ui = UiMainWindow()
     ui.setup_ui(main_window)
     main_window.show()
-    app.exec_()
+    sys.exit(app.exec_())
