a39a6e82c00e8be019f0eb7033c5f82096baf533
Luca Rossi <lucarossi147@gmail.com>
2022-09-07 09:56:27 +0200

    fixed typo

--- src/main.py
+++ src/main.py
@@ -13,12 +13,12 @@ matplotlib.use('Qt5Agg')
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    path_to_file = "../res/Data_CH001_000.abf"
+    path_to_file = "../Data/Data_CH001_000.abf"
     # path_to_file = input("Inserisci il path del file: ")
     abf = fh.open_abf(path_to_file)
-    abf2 = fh.open_abf("../res/Data_CH002_000.abf")
-    abf3 = fh.open_abf("../res/Data_CH003_000.abf")
-    abf4 = fh.open_abf("../res/Data_CH004_000.abf")
+    abf2 = fh.open_abf("../Data/Data_CH002_000.abf")
+    abf3 = fh.open_abf("../Data/Data_CH003_000.abf")
+    abf4 = fh.open_abf("../Data/Data_CH004_000.abf")
 
     if abf is None:
         # TODO show error message
