16c6dc838022d4d5cc4ae4e049fde35d7e815c5b
Luca Rossi <lucarossi147@gmail.com>
2022-09-28 16:43:26 +0200

    added tests for basic logic stuff

--- tests/test_logics2.py
+++ tests/test_logics2.py
@@ -21,6 +21,7 @@ class LogicsTest(unittest.TestCase):
     def test_open_first_abf(self):
         logics_test = Logics2()
         logics_test.open(self.path_to_abf1)
+        self.assertTrue(logics_test.metadata.common_data.channel_count == 2)
         self.assertTrue(len(logics_test.metadata.data) == 2)
 
     def test_common_data_from_abf(self):
@@ -32,7 +33,13 @@ class LogicsTest(unittest.TestCase):
     def test_open_basic_edh(self):
         logics_test = Logics2()
         logics_test.open(self.path_to_basic_edh)
-        print(len(logics_test.metadata.data))
+        self.assertTrue(len(logics_test.metadata.data) == 5)
+        # of all the data only one has the measuring unit mV
+        data_in_mV = list(filter(lambda x: x.measuring_unit == "mV", logics_test.metadata.data))
+        self.assertTrue(len(data_in_mV) == 1)
+        # all the other data has the measuring unit pA
+        data_in_mV = list(filter(lambda x: x.measuring_unit == "pA", logics_test.metadata.data))
+        self.assertTrue(len(data_in_mV) == 4)
 
 
 if __name__ == '__main__':
