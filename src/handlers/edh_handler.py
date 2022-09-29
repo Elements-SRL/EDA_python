9033f4ec024dcc305d66ce63f44af4e8e7085110
Luca Rossi <lucarossi147@gmail.com>
2022-09-29 10:16:05 +0200

    now implementation is satisfying

--- src/handlers/edh_handler.py
+++ src/handlers/edh_handler.py
@@ -1,22 +1,12 @@
 import os
 from typing import List
-
 import numpy as np
 import pyabf
 from pyabf import ABF
-
 from src.data_classes.basic_data import BasicData
 from src.data_classes.common_data import CommonData, SweepType
 from src.data_classes.meta_data import MetaData
-# import abf_handler
 from src.handlers import abf_handler
-
-
-def _are_files_contiguous(file_names: List[str]) -> bool:
-    for f in file_names:
-        if not f.endswith("000.abf"):
-            return True
-    return False
 
 
 def extract_meta_data_from_edh(path_to_file: str, metadata: MetaData):
@@ -30,50 +20,50 @@ def extract_meta_data_from_edh(path_to_file: str, metadata: MetaData):
         for p in prefixes:
             batch_of_files = list(filter(lambda f_name: p in f_name, file_names))
             complete_batch_of_files = list(map(lambda f: dir_path + os.sep + f, batch_of_files))
-            open_contiguous_abf(metadata, complete_batch_of_files)
+            _open_contiguous_abf(metadata, complete_batch_of_files)
     else:
-        open_multiple_abf(path_to_files=file_names, dir_path=dir_path, metadata=metadata)
+        _open_multiple_abf(path_to_files=file_names, dir_path=dir_path, metadata=metadata)
 
 
-def open_multiple_abf(path_to_files: List[str], dir_path: str, metadata: MetaData):
+def _are_files_contiguous(file_names: List[str]) -> bool:
+    for f in file_names:
+        if not f.endswith("000.abf"):
+            return True
+    return False
+
+
+def _open_multiple_abf(path_to_files: List[str], dir_path: str, metadata: MetaData):
     path_to_files.sort()
     for f in path_to_files:
         abs_path = dir_path + os.sep + f
         abf_handler.extract_meta_data_from_abf(metadata=metadata, path_to_file=abs_path)
 
 
-def open_contiguous_abf(metadata: MetaData, path_to_files_of_same_channels: List[str]):
+def _open_contiguous_abf(metadata: MetaData, path_to_files_of_same_channels: List[str]):
     # TODO SAFELY READ ONLY 9 CONTIGUOUS ABFS
     path_to_files_of_same_channels.sort()
     abfs = [pyabf.ABF(p) for p in path_to_files_of_same_channels]
-    first_abf = abfs[0]
-    x = first_abf.sweepX
-    first_abf.setSweep(channel=0, sweepNumber=0)
-    y0 = first_abf.sweepY
-    first_abf.setSweep(channel=1, sweepNumber=0)
-    y1 = first_abf.sweepY
-    i = 0
-    for other in abfs:
-        print(i)
-        i += 1
-        other.setSweep(channel=0, sweepNumber=0)
-        print("canale 0")
-        print(other.sweepUnitsY)
-        x = np.concatenate((x, other.sweepX + x[-1:]), axis=None)
-        y0 = np.concatenate((y0, other.sweepY), axis=None)
-        other.setSweep(channel=1, sweepNumber=0)
-        print("canale 1")
-        print(other.sweepUnitsY)
-        y1 = np.concatenate((y1, other.sweepY), axis=None)
+    basic_data = _extract_data(abfs)
     if metadata.is_empty():
-        metadata.common_data = CommonData(x=x,
-                                          sampling_rate=first_abf.sampleRate,
-                                          channel_count=first_abf.channelCount,
-                                          sweep_type=SweepType.episodic,
-                                          measuring_unit=first_abf.sweepUnitsX)
-    first_abf.setSweep(channel=0, sweepNumber=0)
-    metadata.add_data(BasicData(ch=0, y=y0, measuring_unit=first_abf.sweepUnitsY, sweep_number=0))
-    first_abf.setSweep(channel=1, sweepNumber=0)
-    metadata.add_data(BasicData(ch=1, y=y1, measuring_unit=first_abf.sweepUnitsY, sweep_number=0))
+        metadata.common_data = _extract_common_data(abfs)
+    for data in basic_data:
+        metadata.add_data(data)
 
-# TODO fare un metodo che dati n abf e un canale li sputi fuori una y con tutti gli abf concatenati?
+
+def _extract_common_data(abfs: List[ABF]) -> CommonData:
+    x = abfs.pop(0).sweepX
+    for abf in abfs:
+        x = np.concatenate((x, abf.sweepX + x[-1:]), axis=None)
+    return CommonData(x=x, sampling_rate=abf.sampleRate, channel_count=abf.channelCount,
+                      sweep_type=SweepType.episodic, measuring_unit=abf.sweepUnitsX)
+
+
+def _extract_data(abfs: List[ABF]) -> List[BasicData]:
+    basic_data: List[BasicData] = list()
+    for ch in range(abfs[0].channelCount):
+        y = np.array([])
+        for abf in abfs:
+            abf.setSweep(channel=ch, sweepNumber=0)
+            y = np.concatenate((y, abf.sweepY), axis=None)
+        basic_data.append(BasicData(ch=ch, y=y, measuring_unit=abf.sweepUnitsY, sweep_number=0))
+    return basic_data
