b29a9437fbfcdf535546c6a042d86abb022eccdc
Luca Rossi <lucarossi147@gmail.com>
2022-10-20 17:16:49 +0200

    small main_window refactor

---
+++ src/gui/mpl_canvas.py
@@ -0,0 +1,62 @@
+from typing import List
+
+from matplotlib import pyplot as plt
+from matplotlib.axes import Axes
+from matplotlib.backends.backend_qtagg import FigureCanvasQTAgg
+from matplotlib.figure import Figure
+from matplotlib.widgets import RangeSlider
+
+
+class MplCanvas(FigureCanvasQTAgg):
+    def __init__(self):
+        self.func = None
+        self.slider: RangeSlider | None = None
+        self.fig: Figure = plt.figure()
+        self.ax1 = plt.subplot(211)
+        self.ax2 = plt.subplot(212, sharex=self.ax1)
+        self.ax1.callbacks.connect("xlim_changed", self.on_x_lims_change)
+        self.ax2.callbacks.connect("xlim_changed", self.on_x_lims_change)
+        self.slider_ax = self.fig.add_axes([0.1, 0, 0.8, 0.03])
+        self.only_one_ax = plt.subplot(111)
+        self.active_axis = [self.ax1, self.ax2]
+        super(MplCanvas, self).__init__(self.fig)
+
+    def set_one_plot(self):
+        if self.only_one_ax not in self.fig.axes:
+            self.fig.delaxes(self.ax1)
+            self.fig.delaxes(self.ax2)
+            self.only_one_ax = plt.subplot(111)
+            self.only_one_ax.callbacks.connect("xlim_changed", self.on_x_lims_change)
+        self.active_axis = [self.only_one_ax]
+
+    def set_two_plots(self):
+        if self.only_one_ax in self.fig.axes:
+            self.fig.delaxes(self.only_one_ax)
+            self.ax1 = plt.subplot(211)
+            self.ax2 = plt.subplot(212, sharex=self.ax1)
+            self.ax1.callbacks.connect("xlim_changed", self.on_x_lims_change)
+            self.ax2.callbacks.connect("xlim_changed", self.on_x_lims_change)
+            self.active_axis = [self.ax1, self.ax2]
+        if self.ax1 is not None and self.ax2 is not None:
+            self.ax1.callbacks.connect("xlim_changed", self.on_x_lims_change)
+            self.ax2.callbacks.connect("xlim_changed", self.on_x_lims_change)
+            self.active_axis = [self.ax1, self.ax2]
+
+    def get_active_axis(self) -> List[Axes]:
+        return self.active_axis
+
+    def clean_slider(self):
+        self.fig.delaxes(self.slider_ax)
+        self.slider_ax = self.fig.add_axes([0.1, 0, 0.8, 0.03])
+        if self.slider is not None:
+            self.slider.reset()
+
+    def on_x_lims_change(self, event_ax):
+        self.clean_slider()
+        x_min, x_max = event_ax.get_xlim()
+        self.slider = RangeSlider(self.slider_ax, "Range selector", x_min, x_max)
+        self.slider.on_changed(self.func)
+
+    def set_func(self, func):
+        self.func = func
+
--- src/main_window.py
+++ src/main_window.py
@@ -5,75 +5,19 @@ from PyQt5 import QtWidgets
 from PyQt5.QtCore import QRect, QMetaObject, QCoreApplication, QModelIndex, Qt
 from PyQt5.QtGui import QStandardItemModel, QStandardItem
 from PyQt5.QtWidgets import *
-from matplotlib import pyplot as plt
 from matplotlib.axes import Axes
 from matplotlib.lines import Line2D
 from matplotlib.backends.backend_qtagg import (
-    FigureCanvasQTAgg,
     NavigationToolbar2QT as NavigationToolbar,
 )
-from matplotlib.figure import Figure
 from matplotlib.widgets import RangeSlider
 
 import logics
 from src.gui.filters_widget import FiltersWidget
+from src.gui.mpl_canvas import MplCanvas
 from src.metadata.data_classes.data_group import DataGroup
 
 matplotlib.use("Qt5Agg")
-
-
-class MplCanvas(FigureCanvasQTAgg):
-    def __init__(self):
-        self.func = None
-        self.slider: RangeSlider | None = None
-        self.fig: Figure = plt.figure()
-        self.ax1 = plt.subplot(211)
-        self.ax2 = plt.subplot(212, sharex=self.ax1)
-        self.ax1.callbacks.connect("xlim_changed", self.on_x_lims_change)
-        self.ax2.callbacks.connect("xlim_changed", self.on_x_lims_change)
-        self.slider_ax = self.fig.add_axes([0.1, 0, 0.8, 0.03])
-        self.only_one_ax = plt.subplot(111)
-        self.active_axis = [self.ax1, self.ax2]
-        super(MplCanvas, self).__init__(self.fig)
-
-    def set_one_plot(self):
-        if self.only_one_ax not in self.fig.axes:
-            self.fig.delaxes(self.ax1)
-            self.fig.delaxes(self.ax2)
-            self.only_one_ax = plt.subplot(111)
-            self.only_one_ax.callbacks.connect("xlim_changed", self.on_x_lims_change)
-        self.active_axis = [self.only_one_ax]
-
-    def set_two_plots(self):
-        if self.only_one_ax in self.fig.axes:
-            self.fig.delaxes(self.only_one_ax)
-            self.ax1 = plt.subplot(211)
-            self.ax2 = plt.subplot(212, sharex=self.ax1)
-            self.ax1.callbacks.connect("xlim_changed", self.on_x_lims_change)
-            self.ax2.callbacks.connect("xlim_changed", self.on_x_lims_change)
-            self.active_axis = [self.ax1, self.ax2]
-        if self.ax1 is not None and self.ax2 is not None:
-            self.ax1.callbacks.connect("xlim_changed", self.on_x_lims_change)
-            self.ax2.callbacks.connect("xlim_changed", self.on_x_lims_change)
-            self.active_axis = [self.ax1, self.ax2]
-
-    def get_active_axis(self) -> List[Axes]:
-        return self.active_axis
-
-    def clean_slider(self):
-        self.fig.delaxes(self.slider_ax)
-        self.slider_ax = self.fig.add_axes([0.1, 0, 0.8, 0.03])
-        if self.slider is not None:
-            self.slider.reset()
-
-    def on_x_lims_change(self, event_ax):
-        self.clean_slider()
-        x_min, x_max = event_ax.get_xlim()
-        self.slider = RangeSlider(self.slider_ax, "Range selector", x_min, x_max)
-        self.slider.on_changed(self.func)
-
-    def set_func(self, func):
-        self.func = func
 
 
 def show_empty_abfs_dialog(title, text, informative_text):
