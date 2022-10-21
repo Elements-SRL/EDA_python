185f13f1f3791a57471264aa77aa0df05d74e4ac
Luca Rossi <lucarossi147@gmail.com>
2022-10-21 14:16:01 +0200

    added fitting functionalities

--- tests/test_fitting.py
+++ tests/test_fitting.py
@@ -1,20 +1,26 @@
 import unittest
+
+import numpy as np
+
 from src.analysis.fitting import fitting
 
 
 class TestFitting(unittest.TestCase):
 
     def test_linear_fitting(self):
-        fitting.try_linear_fitting()
+        y, popt = fitting.linear_fitting(np.array([1, 2, 3]), np.array([1, 2, 3]))
 
-    def test_curve_fitting(self):
-        fitting.try_exponential_fitting()
+    def test_quadratic_fitting(self):
+        y, popt = fitting.quadratic_fitting(np.array([1, 2, 3]), np.array([1, 2, 3]))
+
+    def test_exponential_fitting(self):
+        y, popt = fitting.exponential_fitting(np.array([1, 2, 3]), np.array([1, 2, 3]))
 
     def test_power_law_fitting(self):
-        fitting.try_power_law_fitting()
+        y, popt = fitting.power_law_fitting(np.array([1, 2, 3]), np.array([1, 2, 3]))
 
     def test_gaussian_peak_fitting(self):
-        fitting.try_gaussian_peak_fitting()
+        y, popt = fitting.gaussian_fitting(np.array([1, 2, 3]), np.array([1, 2, 3]))
 
 
 if __name__ == '__main__':
