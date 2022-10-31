c753427c99b8aa228718648e6e5086a9d6bd501a
Luca Rossi <lucarossi147@gmail.com>
2022-10-31 11:05:52 +0100

    added sweeps choice

--- src/gui/advanced_roi.py
+++ src/gui/advanced_roi.py
@@ -13,11 +13,22 @@ class AdvancedRoiWidget(QWidget):
         self.setWindowTitle("Advanced ROI")
         self.setMinimumSize(200, 300)
         self.channels_checkbox: List[QCheckBox] = []
-        for bd in dg.basic_data:
-            ch = QCheckBox("channel: " + str(bd.ch))
+        channels_label = QLabel("Select channels to keep")
+        outer_layout.addWidget(channels_label)
+        for ch_number in range(dg.channel_count):
+            ch = QCheckBox("channel: " + str(ch_number))
             ch.setChecked(True)
             outer_layout.addWidget(ch)
             self.channels_checkbox.append(ch)
+        self.sweep_checkbox: List[QCheckBox] = []
+        sweep_label = QLabel("Select sweeps to keep")
+        outer_layout.addWidget(sweep_label)
+        for sweep_number in range(dg.sweep_count):
+            sweep = QCheckBox("sweep: " + str(sweep_number))
+            sweep.setChecked(True)
+            outer_layout.addWidget(sweep)
+            self.sweep_checkbox.append(sweep)
         self.create_roi_button = QPushButton("Create ROI")
+        # TODO add button select/unselect all channels and sweeps
         outer_layout.addWidget(self.create_roi_button)
         self.show()
