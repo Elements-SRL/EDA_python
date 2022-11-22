e391e3e016fcd654c8b71092d3bfb2bddd84a000
Luca Rossi <lucarossi147@gmail.com>
2022-11-22 16:25:42 +0100

    added docs for fitting params

--- src/analysis/fitting/FittingParams.py
+++ src/analysis/fitting/FittingParams.py
@@ -4,7 +4,17 @@ from typing import Tuple, List
 
 @dataclass
 class FittingParams:
-    """Fitting params"""
+    """Fitting params
+
+    Constructor params
+    ------------------
+    ch: int
+        channels for which the params have been found
+    measuring_unit: str
+        measuring unit of channel
+    popt: List[Tuple[str, float]]
+        optimal parameters that approximate the data
+    """
     ch: int
     measuring_unit: str
     popt: List[Tuple[str, float]]
