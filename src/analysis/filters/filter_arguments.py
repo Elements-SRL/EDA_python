51d32b144012ff78f1d04c996888a4e40e5add05
Luca Rossi <lucarossi147@gmail.com>
2022-11-22 12:07:31 +0100

    added docs to filter arguments

--- src/analysis/filters/filter_arguments.py
+++ src/analysis/filters/filter_arguments.py
@@ -3,6 +3,27 @@ from dataclasses import dataclass
 
 @dataclass
 class FilterArguments:
+    """A data class which represents the arguments to pass to a filter
+
+    Attributes
+    ---------
+    fs: float
+        Sampling frequency
+    filter_type: {'butter', 'bessel' 'cheby1'}
+        Specify the type of filter
+    order: int
+        Filter order
+    b_type:  {'lowpass', 'highpass', 'bandpass', 'bandstop'}
+        Band type.
+    cutoff_frequency: float
+        The critical frequency. For a Butterworth filter, this is the point
+        at which the gain drops to 1/sqrt(2) that of the passband (the '-3 dB point').
+    other_cutoff_frequency: float | None = None
+        Another cutoff frequency needed when b_type is 'bandpass' or 'bandstop'.
+    analog: bool = False
+        When True return an analog filter. Default is false.
+    """
+
     fs: float
     filter_type: str
     order: int
