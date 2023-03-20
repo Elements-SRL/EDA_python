a1d4cf71c61f80ee205b94fdc8e5935740134bdf
Luca Rossi <lucarossi147@gmail.com>
2023-03-20 17:42:00 +0100

    fixed broken test

--- tests/test_dwell_analysis.py
+++ tests/test_dwell_analysis.py
@@ -5,27 +5,20 @@ from src.logics.logics import Logics
 class DwellAnalysisTest(unittest.TestCase):
     path_to_abf = "res/EventsDetection/Flowcell 19_external electrodes_PS200 and 350_CH001_000.abf"
 
-    # def test_something(self):
-    #     logics = Logics()
-    #     logics.open(self.path_to_abf)
-    #     dg = logics.metadata.selected_data_group
-    #     logics.dwell_analysis(10, 90)
-    #     # TODO is there a better way to test it?
-    #     assert (len(dg.data_groups) == )
-
     def test_zipped_result(self):
         logics = Logics()
         logics.open(self.path_to_abf)
         results = logics.dwell_analysis(10, 100)
-        a, d, b_e = zip(*results)
+        a, d, b, e = zip(*results)
         assert (len(a) == len(d))
-        assert (len(a) == len(b_e))
+        assert (len(a) == len(b))
+        assert (len(a) == len(e))
 
     def test_get_amplitudes(self):
         logics = Logics()
-        dg = logics.metadata.selected_data_group
         logics.open(self.path_to_abf)
-        logics.dwell_analysis(10, 90)
+        dg = logics.metadata.selected_data_group
+        logics.dwell_analysis(10, 100)
         # TODO is there a better way to test it?
         assert (len(dg.data_groups) == 1)
 
